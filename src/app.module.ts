82194708712ddc5c29d518fd09660ead83c63d09
Jose Armando Fuilco Galindo <josefuilco@gmail.com>
2026-03-27 17:07:21 -0500

    chore: improvement architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { GraphQLConfig } from './core/config/graphql';
+
+import { GraphQLConfig } from './config/graphql';
+
 @Module({
   imports: [GraphQLModule.forRoot(GraphQLConfig)],
   controllers: [],
